4f9fd8fe385eb55d96e628ca3bbdd16df75b8966
LopsanAMO <makiki789@gmail.com>
2016-10-04 22:12:05 -0500

    changes

--- Proyecto/urls.py
+++ Proyecto/urls.py
@@ -1,8 +1,8 @@
 from django.conf.urls import url, include
 from django.contrib import admin
-from blog import urls as blogUrl
+from usuario import urls as usuarioUrl
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
-    url(r'^user/', include(blogUrl, namespace='user'))
+    url(r'^user/', include(usuarioUrl, namespace='user'))
 ]
